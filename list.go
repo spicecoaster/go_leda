e8555794b7e887bf05e36ec6d637c7f8af1d662a
gjkood@yahoo.com <gjkood@yahoo.com>
2015-02-08 12:29:34 -0800

    Add List definition; Add SearchList and InsertList functions.

--- list.go
+++ list.go
@@ -4,4 +4,32 @@ import (
     "fmt"
 )
 
+type ItemType int
+
+type List struct {
+    item ItemType
+    next *List
+}
+
+func SearchList(l *List, x ItemType) *List {
+    if l == nil {
+        return nil
+    }
+
+    if l.item == x {
+        return l
+    } else {
+        return (SearchList(l.next, x))
+    }
+}
+
+func InsertList(l *List, x ItemType) {
+    var p *List
+
+    p = new(List)
+    p.item = x
+    p.next = l
+    l = p
+}
+
 
